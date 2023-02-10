63bd5a1503c6092b690da043d7ca369239169dca
PabloNMerino <pablonicolasm@hotmail.com>
2023-02-10 10:45:46 -0300

    actualizacion

--- Alien vs Humans/src/AlienType.java
+++
@@ -1,3 +0,0 @@
-public enum AlienType {
-    ROGUE, TANK, ARMORMASTER
-}
--- Alien vs Humans/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Human soldier_one = new Human("Pablo", 25);
-        String className = soldier_one.getClass().getName();
-
-    }
-}
